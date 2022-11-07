fba79a2f6ebd7d15269f6d6f46c7558efe215d8a
Emmvid <emmavidarsson@hotmail.com>
2022-11-07 22:13:51 +0100

    updated cart function, not quite working yet..

--- script.js
+++ script.js
@@ -5,11 +5,15 @@ let shoppingCart = document.getElementsByClassName(".shopping-cart")
 let beers = [];
 let cart = (localStorage.getItem("data")) || [];
 
+
 document.addEventListener("DOMContentLoaded", (event) => {
   fetch(url)
-  .then(res => {return res.json()})
+  .then(res => {
+    return res.json()
+  })
   .then(data => {
     console.log(data);
+
     for(let i=0; i < data.length; i++){
       beers.push(data[i])
     }
@@ -18,7 +22,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
     for(let i = 0; i < beers.length; i++){
       if( i % 3 == 0 && i > 0)
       {
-        console.log(i)
+        //console.log(i)
         cardContent += `</div> <br>
         <div class"row my-2">`
       }
@@ -29,7 +33,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
         <h5 class="card-title"> ${beers[i].name + " " + i}</h5>
         <p class="card-text">${beers[i].description}</p>
         <p class="card-text"> Alchol amount: ${beers[i].abv} % </p>
-        <h6 class="card-subtitle text-muted text-center">${Math.floor(Math.random() * (35 - 20) + 20)}:-</h6>
+        <h6 class="card-subtitle text-muted text-center">Price: ${Math.floor(Math.random() * (35 - 20) + 20)}:-</h6>
         <div id=${i} class="add btn btn-primary mt-2">Add to cart</div>
       </div>
     </div>
@@ -42,13 +46,49 @@ document.addEventListener("DOMContentLoaded", (event) => {
     addButtons.forEach(btn => {
       btn.addEventListener('click', (event) =>{
         cart.push(beers[event.target.id]);
-        console.log(cart)
-        localStorage.setItem('cart' , JSON.stringify(cart))
-        let itemInStorage = localStorage.getItem('cart');
-        console.log('items In Storage ', JSON.parse(itemInStorage));
-
+        
+        cartNumbers(cart);
+        
       })
     })
   })
- 
+  .catch((error) => console.log("ERROR" + error));
 })
+//funktion för att uppdatera det som finns i varukorgen
+function reloadCartNumbers () {
+  let productNumbers = localStorage.getItem('cartNumbers');
+
+  if (productNumbers) {
+    document.querySelector(".cart span").textContent = productNumbers;
+  }
+}
+
+//Lägger till det man klickar på i varukorgen
+function cartNumbers(cart) {
+  let productNumbers = localStorage.getItem('cartNumbers');
+  productNumbers = parseInt(productNumbers);
+  if(productNumbers) {
+    localStorage.setItem('cartNumbers', productNumbers +1);
+    document.querySelector(".cart span").textContent = productNumbers +1;
+  }
+  else {
+    localStorage.setItem('cartNumbers', 1);
+    document.querySelector(".cart span").textContent  = 1
+  }
+  setItems(cart)
+}
+
+function setItems(cart) {
+  let cartItems = localStorage.getItem('productsInCart');
+  cartItems = JSON.parse(cartItems);
+  console.log("my cartItems are", cartItems)
+  if (cartItems != null) {
+    cartItems[cart.i].inCart += 1;
+  }
+  else {
+    cart.inCart =1;
+    cartItems = {
+      [cart.i]: cart
+    }
+  }
+}

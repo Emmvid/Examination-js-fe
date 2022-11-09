fc6df2a674f71d742fbae998e4f880647e7c9f59
Emmvid <emmavidarsson@hotmail.com>
2022-11-09 20:09:04 +0100

    didn't get the cart to work so made another thing

--- script.js
+++ script.js
@@ -1,93 +1,105 @@
 "use strict";
-const url = "https://api.punkapi.com/v2/beers?20";
-let shoppingCart = document.getElementsByClassName(".shopping-cart");
+const main = document.querySelector("main");
+const loadMoreBeer = document.querySelector("#load-beer");
+const url = "https://api.punkapi.com/v2/beers/";
+let wishList;
+let beerPrice = Math.floor(Math.random() * (35 - 20) + 20);
 
-let beers = [];
-let cart = localStorage.getItem("data") || [];
+if (localStorage.getItem("wishList")){
+  wishList = JSON.parse(localStorage.getItem("wishList"));
+} else {
+  wishList = [];
+}
+
+function getBeer(id) {
+  fetch(url + id)
+  .then((res) => res.json())
+  .then((data)=> displayBeer(data[0]));
+}
+function displayBeer(data) {
+  const article = document.createElement("article");
+  article.classList.add("card");
+  article.innerHTML =` <div class="card row-md-2">
+  <img src="${data.image_url}" style="height: 200px; width: 80px; align-items: center;" alt="Picture of the beer">
+  <h2 class="card-title text-center">${data.name}</h2>
+  <h4 class="card-text py-1 text-muted">${data.tagline}</h4>
+  <p class="card-text py-1"> ${data.description}
+  <h6 class="card-subtitle text-muted text-center">${beerPrice}:-</h6>
+  <p class="card-text py-1">Alcohol: ${data.abv} % </p>
+
+</div>
+</div>
+</div> `;
+
+  const button = document.createElement("button");
+  button.classList.add("btn", "btn-info");
+  button.innerText= "Add to Wishlist";
+  button.onclick = function (){
+    wishList.push(data);
+    localStorage.setItem("wishList", JSON.stringify(wishList));
+    displayWishList();
+  }
+  article.querySelector(".card").append(button);
+  main.append(article);
+}
+
+function displayWishList() {
+  let list = document.querySelector(".offcanvas-body");
+  list.innerHTML= "";
+
+  for(let beer of wishList){
+    const addText = document.createElement("p");
+    const removeItem = document.createElement("button");
+    addText.innerHTML = `<img src="${beer.image_url}" style="height: 100px; width: 40px;" alt="picture of beer"> ${beer.name} Price: ${beerPrice}:- `;
+    list.appendChild(addText);
+    removeItem.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+    <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
+  </svg>`;
+    removeItem.classList.add("removeButton");
+    addText.appendChild(removeItem);
+  removeItem.addEventListener("click", () => {
+    removeItem.parentElement.remove();
+    wishList.splice(wishList.indexOf(beer), 1);
+    localStorage.setItem("wishList", JSON.stringify(wishList))
+  });
+  }
+  function cartNumbers(wishList) {
+  
+    let productNumbers = localStorage.getItem("cartNumbers");
+    productNumbers = parseInt(productNumbers);
+    if (productNumbers) {
+      localStorage.setItem("cartNumbers", productNumbers + 1);
+      document.querySelector(".btn span").textContent = productNumbers + 1;
+    } else {
+      localStorage.setItem("cartNumbers", 1);
+      document.querySelector(".btn span").textContent = 1;
+    } 
+  }
+  function reloadCartNumbers(wishList) {
+    let productNumbers = localStorage.getItem("cartNumbers");
+  
+    if (productNumbers) {
+      document.querySelector(".btn span").textContent = productNumbers;
+    }
+  }
+  cartNumbers(wishList);
+  reloadCartNumbers(wishList);
+}
+
+function showMoreBeer(start, end) {
+  for(let i= start; i< end; i++) {
+    getBeer(i);
+  }
+}
 
 document.addEventListener("DOMContentLoaded", (event) => {
-  fetch(url)
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      console.log(data);
+  showMoreBeer(1, 21)
+})
 
-      for (let i = 0; i < data.length; i++) {
-        beers.push(data[i]);
-      }
-      let cardContainer = document.querySelector("#cardContainer");
-      let cardContent = `<div class="row my-2">`;
-      for (let i = 0; i < beers.length; i++) {
-        if (i % 3 == 0 && i > 0) {
-          //console.log(i)
-          cardContent += `</div> <br>
-        <div class"row my-2">`;
-        }
-        cardContent += `<div class="card-group">
-      <div class="card mt-4 ml-4 p-3" style="width: 18rem;  ">
-      <img class="card-img-top" src="${
-        beers[i].image_url
-      }" style="height: 200px; width: 80px; align-items: center;" alt="Picture of the beer">
-      <div class="card-body">
-        <h5 class="card-title"> ${beers[i].name + " " + i}</h5>
-        <p class="card-text">${beers[i].description}</p>
-        <p class="card-text"> Alchol amount: ${beers[i].abv} % </p>
-        <h6 class="card-subtitle text-muted text-center">Price: ${Math.floor(
-          Math.random() * (35 - 20) + 20
-        )}:-</h6>
-        <div id=${i} class="add btn btn-primary mt-2">Add to cart</div>
-      </div>
-    </div>
-    </div>`;
-      }
-      cardContainer.innerHTML += cardContent;
-    })
-    .then(() => {
-      let addButtons = document.querySelectorAll(".add");
-      addButtons.forEach((btn) => {
-        btn.addEventListener("click", (event) => {
-          cart.push(beers[event.target.id]);
+let loadedAmount = 20;
 
-          cartNumbers(cart);
-        });
-      });
-    })
-    .catch((error) => console.log("ERROR" + error));
-});
-//funktion för att uppdatera det som finns i varukorgen
-function reloadCartNumbers() {
-  let productNumbers = localStorage.getItem("cartNumbers");
-
-  if (productNumbers) {
-    document.querySelector(".cart span").textContent = productNumbers;
-  }
-}
-
-//Lägger till det man klickar på i varukorgen
-function cartNumbers(cart) {
-  let productNumbers = localStorage.getItem("cartNumbers");
-  productNumbers = parseInt(productNumbers);
-  if (productNumbers) {
-    localStorage.setItem("cartNumbers", productNumbers + 1);
-    document.querySelector(".cart span").textContent = productNumbers + 1;
-  } else {
-    localStorage.setItem("cartNumbers", 1);
-    document.querySelector(".cart span").textContent = 1;
-  }
-  setItems(cart);
-}
-
-function setItems(cart) {
-  let cartItems = localStorage.getItem("productsInCart");
-  cartItems = JSON.parse(cartItems);
-  console.log("my cartItems are", cartItems);
-  if (cartItems != null) {
-    cartItems[cart.i].inCart += 1;
-  } else {
-    cart.inCart = 1;
-    cartItems = {
-      [cart.i]: cart,
-    };
-  }
+loadMoreBeer.onclick = function () {
+  if (loadedAmount >= 100) return;
+  showMoreBeer(loadedAmount + 1, loadedAmount +21)
+  loadedAmount +=20
 }
